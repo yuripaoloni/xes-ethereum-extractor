6869addeb05bbafde47d0ce563f13530c09a30dd
yuripaoloni <yuri.paoloni3@gmail.com>
2022-04-14 15:15:45 +0200

    feat: export txs in JSON

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,13 @@
-import  json
+from inspect import _void
+import os,json
 
 def get_contract_address(ethereum_network: str, contract_name: str ) -> str:
-    with open("./constants/addresses.json") as addresses_file :
+    with open("constants/addresses.json") as addresses_file :
         addresses_list = json.load(addresses_file)
     
     return addresses_list[ethereum_network][contract_name]
+
+def export_transactions(transactions: dict, filename: str) -> _void:
+    os.makedirs("data", exist_ok=True)
+    with open(f"data/{filename}.json", "w", encoding="utf-8") as f:
+        json.dump(transactions, f, ensure_ascii=False, indent=2)
