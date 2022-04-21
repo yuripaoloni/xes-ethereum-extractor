9787729de2af2ce20fc1581c9db1d2eb038913b6
yuripaoloni <yuri.paoloni3@gmail.com>
2022-04-21 10:12:08 +0200

    perf: specify input XES file

--- scripts/discovery.py
+++ scripts/discovery.py
@@ -1,14 +1,16 @@
+import sys
+
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
-
 from pm4py.objects.log.importer.xes import importer as xes_importer
-
 from pm4py.objects.conversion.process_tree import converter as pt_converter
-
 from pm4py.visualization.process_tree import visualizer as pt_visualizer
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
 
+# XES file
+file_path = sys.argv[1]
+
 # import xes log
-log = xes_importer.apply("data/log.xes")
+log = xes_importer.apply(file_path)
 
 # process tree
 tree = inductive_miner.apply_tree(log)
