4f701d8c6e7ae1948e1c83b729733db7f97d5f9a
yuripaoloni <yuri.paoloni3@gmail.com>
2022-05-03 14:11:52 +0200

    feat: get txs from multiple contracts

--- scripts/transactions.py
+++ scripts/transactions.py
@@ -18,70 +18,79 @@ eth = Etherscan(ETHERSCAN_API_KEY)
 PROVIDER_ENDPOINT = os.getenv("INFURA_ENDPOINT")
 w3 = Web3(Web3.HTTPProvider(PROVIDER_ENDPOINT))
 
-# get contracts addresses from /constants/addresses.json
-LAND_PROXY_ADDRESS = get_contract_address("mainnet", "LANDProxy")
-LAND_REGISTRY_ADDRESS = get_contract_address("mainnet", "LANDRegistry")
+# define list of contracts to analyze by specifying the contract on which get txs and the contract for decoding the abi
+contracts = []
 
-# get contracts ABI
-land_registry_abi = eth.get_contract_abi(LAND_REGISTRY_ADDRESS)
-
-# create a contract instance to decode transactions input
-land_registry_contract = w3.eth.contract(
-    w3.toChecksumAddress(LAND_REGISTRY_ADDRESS), abi=land_registry_abi)
+contracts.append({"name": "LAND", "txs_contract": get_contract_address(
+    "mainnet", "LANDProxy"), "abi_contract": get_contract_address("mainnet", "LANDRegistry")})
+contracts.append({"name": "ESTATE", "txs_contract": get_contract_address(
+    "mainnet", "EstateProxy"), "abi_contract": get_contract_address("mainnet", "EstateRegistry")})
+contracts.append({"name": "Marketplace", "txs_contract": get_contract_address(
+    "mainnet", "MarketplaceProxy"), "abi_contract": get_contract_address("mainnet", "Marketplace")})
 
 # will contain all the retrieved transactions
-land_proxy_transactions = []
+results = []
 
-# ? one year block: 12322265
-blockNumber = 12322265
+for contract in contracts:
+    print(f"Contract: {contract['name']}")
 
-# the Etherscan APIs returns max 10000 results. The "while true" loop allows to get all the transactions from an address
-# Retrieve 10000 a time and append them on 'land_proxy_transactions'. Stop when the length of the retrieved transactions is 1
-while True:
+    # ? one year block: 12322265
+    blockNumber = 12322265
 
-    print(f"Block number: {blockNumber}")
+    # get contracts ABI
+    contract_abi = eth.get_contract_abi(contract["abi_contract"])
 
-    # retrieve transactions within the specified block range (max 10000 transactions)
-    transactions = eth.get_normal_txs_by_address(
-        LAND_PROXY_ADDRESS, blockNumber, 99999999, "asc")
+    # create a contract instance to decode transactions input
+    contract_instance = w3.eth.contract(
+        w3.toChecksumAddress(contract["abi_contract"]), abi=contract_abi)
 
-    print(f"{len(transactions)} transactions retrieved")
+    # the Etherscan APIs returns max 10000 results. The "while true" loop allows to get all the transactions from an address
+    # Retrieve 10000 a time and append them on 'results'. Stop when the length of the retrieved transactions is 1
+    while True:
 
-    # if the transactions length is 1 means that we reached the last block with a transaction to the specified address
-    if(len(transactions) == 1):
-        break
+        print(f"\tBlock number: {blockNumber}")
 
-    # iterate through transactions to decode input and get function signature (function) and params (function_params)
-    for transaction in transactions:
+        # retrieve transactions within the specified block range (max 10000 transactions)
+        transactions = eth.get_normal_txs_by_address(
+            contract["txs_contract"], blockNumber, 99999999, "asc")
 
-        # decode input of valid transactions
-        if(transaction["isError"] == "0"):
+        print(f"\t{len(transactions)} transactions retrieved")
 
-            # in case of 'ValueError' exceptions, the execution goes on. This kind of exceptions are thrown when the input isn't decodable
-            # happens with the deploy transaction and with the function of LANDProxy like 'upgrade'.
-            with suppress(ValueError):
-                function, function_params = land_registry_contract.decode_function_input(
-                    transaction["input"])
-                transaction["inputFunction"] = str(function)
-                transaction["inputFunctionName"] = str(function.fn_name)
+        # if the transactions length is 1 means that we reached the last block with a transaction to the specified address
+        if(len(transactions) == 1):
+            break
 
-                # bytes value must be decoded in utf-8
-                for key, value in function_params.items():
-                    if isinstance(value, bytes):
-                        function_params[key] = function_params[key].decode(
-                            "utf-8", "ignore")
+        # iterate through transactions to decode input and get function signature (function) and params (function_params)
+        for transaction in transactions:
 
-                transaction["inputFunctionParams"] = str(function_params)
+            # decode input of valid transactions
+            if(transaction["isError"] == "0"):
 
-        # remove failed transactions
-        else:
-            transactions.remove(transaction)
+                # in case of 'ValueError' exceptions, the execution goes on. This kind of exceptions are thrown when the input isn't decodable
+                # happens with the deploy transaction and with functions of Proxy contracts like 'upgrade'.
+                with suppress(ValueError):
+                    function, function_params = contract_instance.decode_function_input(
+                        transaction["input"])
+                    transaction["inputFunction"] = str(function)
+                    transaction["inputFunctionName"] = str(function.fn_name)
 
-    # save block number of lastest element in the transactions list
-    blockNumber = transactions[-1]["blockNumber"]
+                    # bytes value must be decoded in utf-8
+                    for key, value in function_params.items():
+                        if isinstance(value, bytes):
+                            function_params[key] = function_params[key].decode(
+                                "utf-8", "ignore")
 
-    # append transactions retrieved in the current iteration to the total list of transactions
-    land_proxy_transactions.extend(transactions)
+                    transaction["inputFunctionParams"] = str(function_params)
+
+            # remove failed transactions
+            else:
+                transactions.remove(transaction)
+
+        # save block number of lastest element in the transactions list
+        blockNumber = transactions[-1]["blockNumber"]
+
+        # append transactions retrieved in the current iteration to the total list of transactions
+        results.extend(transactions)
 
 # export JSON file
-export_dictionary(land_proxy_transactions, "data/transactions", sys.argv[1])
+export_dictionary(results, "data/transactions", sys.argv[1])
