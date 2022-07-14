f206fa9f91a1f62164892f3d94b78af3cf6dbb31
yuripaoloni <yuri.paoloni3@gmail.com>
2022-07-14 11:52:53 +0200

    feat: notebook with full framework execution

--- scripts/traces.py
+++ scripts/traces.py
@@ -5,55 +5,48 @@ from pm4py.objects.log.util import dataframe_utils
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 
-# ? params: input file
 
-# transactions JSON file
-file_path = sys.argv[1]
+def clean_df(df):
+    # remove unnecessary fields
+    df.drop(["blockNumber", "nonce", "blockHash", "value", "gas", "gasPrice", "isError", "txreceipt_status", "input",
+             "contractAddress", "cumulativeGasUsed", "gasUsed",  "confirmations", "methodId"], axis=1, inplace=True)
 
-# import json file and sort by 'timeStamp' value
-transactions_df = pd.read_json(file_path)
-transactions_df = dataframe_utils.convert_timestamp_columns_in_df(
-    transactions_df)
-transactions_df = transactions_df.sort_values(
-    by=["timeStamp", "transactionIndex"])
+    df = dataframe_utils.convert_timestamp_columns_in_df(df)
+    df = df.sort_values(by=['timeStamp', 'transactionIndex'])
 
-# add 1 second to timestamp to make them different
-last_timestamp = ""
-counter = 1
-
-for index, row in transactions_df.iterrows():
-    if(row["timeStamp"] == last_timestamp):
-        counter = counter + 1
-        new_timestamp = pd.to_datetime(
-            row["timeStamp"] + pd.to_timedelta(counter, unit='s'))
-    else:
-        last_timestamp = row["timeStamp"]
-        counter = 1
-        new_timestamp = pd.to_datetime(
-            row["timeStamp"] + pd.to_timedelta(counter, unit='s'))
-
-    transactions_df.at[index, 'TIMESTAMP'] = new_timestamp
-
-# remove unnecessary fields
-transactions_df.drop(["nonce", "gas", "gasPrice", "isError", "txreceipt_status",
-                     "contractAddress", "gasUsed", "cumulativeGasUsed", "confirmations"], axis=1, inplace=True)
-
-# drop null values (in case any)
-transactions_df.dropna(inplace=True)
+    return df
 
 
-# create columns: from -> case:concept:name, inputFunctionName -> concept:name, timeStamp -> time:timestamp, from -> org:resource
-transactions_df["org:resource"] = transactions_df["from"]
-transactions_df["case:concept:name"] = transactions_df["from"]
-transactions_df["time:timestamp"] = transactions_df["timeStamp"]
-transactions_df["concept:name"] = transactions_df["inputFunctionName"]
+def sort_df(df, columns: list[str]):
+    df = df.sort_values(by=columns)
+    df.reset_index(drop=True, inplace=True)  # needed after sorting
 
-# specify that the field identifying the case identifier attribute is the field with name 'case:concept:name'
-parameters = {
-    log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'case:concept:name'}
-log = log_converter.apply(transactions_df, parameters=parameters,
-                          variant=log_converter.Variants.TO_EVENT_LOG)
+    return df
 
-# export generated events log in xes
-xes_exporter.apply(
-    log, f"data/logs/{os.path.splitext(os.path.basename(file_path))[0]}.xes")
+
+def generate_xes(df, filename, case_concept_name, concept_name):
+
+    # create columns: from -> case:concept:name, inputFunctionName -> concept:name, timeStamp -> time:timestamp, from -> org:resource
+    df["org:resource"] = df["from"]
+    df["case:concept:name"] = df[case_concept_name]
+    df["time:timestamp"] = df["timeStamp"]
+    df["concept:name"] = df[concept_name]
+
+    # specify that the field identifying the case identifier attribute is the field with name 'case:concept:name'
+    parameters = {
+        log_converter.Variants.TO_EVENT_LOG.value.Parameters.CASE_ID_KEY: 'case:concept:name'}
+    log = log_converter.apply(df, parameters=parameters,
+                              variant=log_converter.Variants.TO_EVENT_LOG)
+
+    pd.options.mode.use_inf_as_na = True  # consider NA also "" or '' during notna()
+
+    # remove "nan" attributes from events
+    for t in log:
+        events += len(t)
+        for i, e in enumerate(t):
+            t[i] = {k: v for k, v in e.items() if pd.Series(
+                v).notna().all()}
+
+    # export generated events log in xes
+    xes_exporter.apply(
+        log, f"../data/logs/{os.path.splitext(os.path.basename(filename))[0]}.xes")
--- scripts/transactions.py
+++ scripts/transactions.py
@@ -1,95 +1,89 @@
 import os
-import sys
 
 from contextlib import suppress
 from dotenv import load_dotenv
 from etherscan import Etherscan
 from web3 import Web3
-from utils import get_contract_address, export_dictionary
 
-# ? params: output file name
 
-load_dotenv()
+def get_transactions(contracts, start_block, end_block):
 
-# initialize etherscan-python and web3.py with respective API keys
-ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
-eth = Etherscan(ETHERSCAN_API_KEY)
-PROVIDER_ENDPOINT = os.getenv("INFURA_ENDPOINT")
-w3 = Web3(Web3.HTTPProvider(PROVIDER_ENDPOINT))
+    # initialize etherscan-python and web3.py with respective API keys
+    load_dotenv()
+    ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
+    eth = Etherscan(ETHERSCAN_API_KEY)
+    PROVIDER_ENDPOINT = os.getenv("INFURA_ENDPOINT")
+    w3 = Web3(Web3.HTTPProvider(PROVIDER_ENDPOINT))
 
-# define list of contracts to analyze by specifying the contract on which get txs and the contract for decoding the abi
-contracts = []
+    # will contain all the retrieved transactions
+    results = []
 
-contracts.append({"name": "LAND", "txs_contract": get_contract_address(
-    "mainnet", "LANDProxy"), "abi_contract": get_contract_address("mainnet", "LANDRegistry")})
-contracts.append({"name": "ESTATE", "txs_contract": get_contract_address(
-    "mainnet", "EstateProxy"), "abi_contract": get_contract_address("mainnet", "EstateRegistry")})
-contracts.append({"name": "Marketplace", "txs_contract": get_contract_address(
-    "mainnet", "MarketplaceProxy"), "abi_contract": get_contract_address("mainnet", "Marketplace")})
+    for contract in contracts:
+        print(f"Contract: {contract['name']}")
 
-# will contain all the retrieved transactions
-results = []
+        block_number = start_block
 
-for contract in contracts:
-    print(f"Contract: {contract['name']}")
+        # get contracts ABI
+        contract_abi = eth.get_contract_abi(contract["abi_contract"])
 
-    # ? one year block: 12322265
-    blockNumber = 12322265
+        # create a contract instance to decode transactions input
+        contract_instance = w3.eth.contract(
+            w3.toChecksumAddress(contract["abi_contract"]), abi=contract_abi)
 
-    # get contracts ABI
-    contract_abi = eth.get_contract_abi(contract["abi_contract"])
+        # the Etherscan APIs returns max 10000 results. The "while true" loop allows to get all the transactions from an address
+        # Retrieve 10000 a time and append them on 'results'. Stop when the length of the retrieved transactions is 1
+        while True:
 
-    # create a contract instance to decode transactions input
-    contract_instance = w3.eth.contract(
-        w3.toChecksumAddress(contract["abi_contract"]), abi=contract_abi)
+            print(f"\tBlock number: {block_number}")
 
-    # the Etherscan APIs returns max 10000 results. The "while true" loop allows to get all the transactions from an address
-    # Retrieve 10000 a time and append them on 'results'. Stop when the length of the retrieved transactions is 1
-    while True:
+            # retrieve transactions within the specified block range (max 10000 transactions)
+            transactions = eth.get_normal_txs_by_address(
+                contract["txs_contract"], block_number, end_block, "asc")
 
-        print(f"\tBlock number: {blockNumber}")
+            print(f"\t{len(transactions)} transactions retrieved")
 
-        # retrieve transactions within the specified block range (max 10000 transactions)
-        transactions = eth.get_normal_txs_by_address(
-            contract["txs_contract"], blockNumber, 99999999, "asc")
+            last_transaction = transactions[-1] if transactions else None
+            last_result = results[-1] if results else None
 
-        print(f"\t{len(transactions)} transactions retrieved")
+            # if last_transaction hash = last_result hash, it means we reached the end
+            if(last_transaction is not None and last_result is not None and last_transaction['hash'] == last_result['hash']):
+                break
 
-        # if the transactions length is 1 means that we reached the last block with a transaction to the specified address
-        if(len(transactions) == 1):
-            break
+            # iterate through transactions to decode input and get function signature (function) and params (function_params)
+            # transactions[:] is a copy transactions. We iterate on a copy to not screw up the list with transactions.remove() below
+            for transaction in transactions[:]:
 
-        # iterate through transactions to decode input and get function signature (function) and params (function_params)
-        for transaction in transactions:
+                # decode input of valid transactions
+                if(transaction["isError"] == "0"):
 
-            # decode input of valid transactions
-            if(transaction["isError"] == "0"):
+                    # in case of 'ValueError' exceptions, the execution goes on. This kind of exceptions are thrown when the input isn't decodable
+                    # happens with the deploy transaction and with functions of Proxy contracts like 'upgrade'.
+                    with suppress(ValueError):
+                        function, function_params = contract_instance.decode_function_input(
+                            transaction["input"])
+                        transaction["inputFunctionName"] = str(
+                            function.fn_name)
 
-                # in case of 'ValueError' exceptions, the execution goes on. This kind of exceptions are thrown when the input isn't decodable
-                # happens with the deploy transaction and with functions of Proxy contracts like 'upgrade'.
-                with suppress(ValueError):
-                    function, function_params = contract_instance.decode_function_input(
-                        transaction["input"])
-                    transaction["inputFunction"] = str(function)
-                    transaction["inputFunctionName"] = str(function.fn_name)
+                        # bytes value must be decoded in utf-8
+                        for key, value in function_params.items():
+                            if isinstance(value, bytes):
+                                function_params[key] = function_params[key].decode(
+                                    "utf-8", "ignore")
 
-                    # bytes value must be decoded in utf-8
-                    for key, value in function_params.items():
-                        if isinstance(value, bytes):
-                            function_params[key] = function_params[key].decode(
-                                "utf-8", "ignore")
+                            transaction[f"{contract['name']}_{key}"] = str(
+                                value)
 
-                    transaction["inputFunctionParams"] = str(function_params)
+                        transaction["inputFunctionParams"] = str(
+                            function_params)
 
-            # remove failed transactions
-            else:
-                transactions.remove(transaction)
+                # remove failed transactions
+                else:
+                    transactions.remove(transaction)
 
-        # save block number of lastest element in the transactions list
-        blockNumber = transactions[-1]["blockNumber"]
+            # save block number of lastest element in the transactions list
+            block_number = transactions[-1]["blockNumber"]
 
-        # append transactions retrieved in the current iteration to the total list of transactions
-        results.extend(transactions)
+            # append transactions retrieved in the current iteration to the total list of transactions
+            results.extend(transactions)
 
-# export JSON file
-export_dictionary(results, "data/transactions", sys.argv[1])
+    return results
