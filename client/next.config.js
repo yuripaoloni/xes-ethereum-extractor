d6a30307f524550319d2593788401933b193c107
yuripaoloni <yuri.paoloni3@gmail.com>
2022-09-06 09:57:40 +0200

    build: .env variables in nex.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
